8ecd8439b5534185224e02f89005e9a5252586bc
SonHyeonJeong <135013756+aaliuqet@users.noreply.github.com>
2025-04-09 04:13:37 +0900

    Update wordcloud_japan.py

--- 분석 및 시각화/워드클라우드 및 워드카운트/wordcloud_japan.py
+++ 분석 및 시각화/워드클라우드 및 워드카운트/wordcloud_japan.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 import re
 
 # MongoDB 서버에 접속해서 필요한 정보를 불러옵니다.
-client = pymongo.MongoClient(host="1.234.51.110", port=38019, username='clawling', password='goodtime**95')
+client = pymongo.MongoClient(host="", port=, username='', password='')
 db = client['OneAsia2024']
 
 # 데이터 불러오기
