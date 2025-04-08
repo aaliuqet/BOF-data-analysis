26177b09174105586992ff3ac72eb1863b8c3c70
SonHyeonJeong <135013756+aaliuqet@users.noreply.github.com>
2025-04-09 04:14:03 +0900

    Update wordcloud_korea.py

--- 분석 및 시각화/워드클라우드 및 워드카운트/wordcloud_korea.py
+++ 분석 및 시각화/워드클라우드 및 워드카운트/wordcloud_korea.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 # MongoDB 서버에 접속해서 필요한 정보를 불러옵니다.
-client = pymongo.MongoClient(host="1.234.51.110", port=38019, username='clawling', password='goodtime**95')
+client = pymongo.MongoClient(host="", port=, username='', password='')
 db = client['OneAsia2024']
 
 # 데이터 불러오기
